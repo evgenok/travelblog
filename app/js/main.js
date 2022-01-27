637df227f5183729854dee4cccc61690d390992e
evgenok <popovevgen18042001@mail.ru>
2022-01-27 23:19:44 +0300

    added scroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
-$(function() {
-    
-})
+$(function () {
+     $(".container a, .go-top").on("click", function (event) {
+
+          event.preventDefault();
+          var id = $(this).attr('href'),
+          top = $(id).offset().top;
+          $('body, html').animate({scrollTop: top}, 1000);
+     })
+});
